b194fb100748f31e01b7243cb09a72dd1263b75f
kentoshiohara <120357291+kentoshiohara@users.noreply.github.com>
2023-03-09 22:56:19 +0900

    categoryテーブルの作成

---
+++ app/controllers/categories_controller.rb
@@ -0,0 +1,12 @@
+class CategoriesController < ApplicationController
+  
+  def index
+  end
+
+  def new
+  end
+
+  def create
+  end
+  
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_03_08_130415) do
+ActiveRecord::Schema.define(version: 2023_03_09_082827) do
+
+  create_table "categories", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
+    t.string "title", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_categories_on_user_id"
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
     t.string "name", default: "", null: false
@@ -25,4 +33,5 @@ ActiveRecord::Schema.define(version: 2023_03_08_130415) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "categories", "users"
 end
