a5ce0ca0fca5d2966bb3420158a80c981e376445
kentoshiohara <120357291+kentoshiohara@users.noreply.github.com>
2023-03-10 21:06:16 +0900

    コントローラーの修正

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,12 +1,24 @@
 class CategoriesController < ApplicationController
   
   def index
+    @category = Category.new
+    
   end
 
   def new
+    # @category = Category.new
   end
 
   def create
+    category = Category.new(category_params)
+    if category.save
+      render :index
+    end
   end
-  
+
+  private
+  def category_params
+    params.require(:category).permit(:title).merge(user_id: current_user.id)
+  end
+
 end
